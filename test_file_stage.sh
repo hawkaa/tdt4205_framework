541111228deda73d4e0dd6105974f418b55e4dd1
Håkon Åmdal <hakon@aamdal.com>
2014-02-16 10:43:13 +0100

    nytt testscript

---
+++ test_file_stage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+make;
+mkdir -p test_file_stage.d;
+cp "correct_output/$1.s$2" "test_file_stage.d/$1.s$2"
+
+./bin/vslc -s $2 < vsl_programs/$1.vsl 2> test_file_stage.d/$1.test.s$2
+
+echo ""
+echo "-- CORRECT OUTPUT --"
+cat test_file_stage.d/$1.s$2
+
+echo ""
+echo ""
+echo "-- YOUR OUTPUT --"
+cat test_file_stage.d/$1.test.s$2
+
