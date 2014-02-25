5d2f240fd7d6da1a933529fb8ff0fa077c57f03e
Håkon Åmdal <hakon@aamdal.com>
2014-02-25 11:19:29 +0100

    bind names

--- src/bindnames.c
+++ src/bindnames.c
@@ -9,6 +9,8 @@ extern int outputStage;
 
 char* thisClass;
 
+static const int OFFSET_SIZE = 4;
+
 /*
  * Default symtab binding function.
  * Will recursively call the corresponding binding function on its children,
@@ -46,20 +48,30 @@ bind_constant(node_t *root, int stackOffset)
 int
 bind_function(node_t *root, int stackOffset)
 {
-	int offset = -4;
+	
+	int automatic_var_offset, formal_var_offset;
 
 	if(outputStage == 6)
 		fprintf ( stderr, "FUNCTION: Start: %s\n", root->label);
 
-	/* first child */
-	if(root->children[0] != NULL)
-		root->children[0]->bind_names(root->children[0], 0);
+	/* variable list */
+	node_t *vl = root->children[0];
+	if(vl != NULL) {
+		formal_var_offset = stackOffset + vl->n_children - 1;
+		for(int i = 0; i < vl->n_children; ++i) {
+			vl->children[i]->bind_names(vl->children[i], formal_var_offset*OFFSET_SIZE);
+			--formal_var_offset;
+		}
+	}
+		
+
 	/* function statement list */
 	node_t *sl = root->children[1];
+	automatic_var_offset = -1;
 	for(int i=0; i < sl->n_children; ++i) {
 		if(sl->children[i]->nodetype.index == DECLARATION_STATEMENT) {
-			sl->children[i]->bind_names(sl->children[i], offset);
-			offset -= 4;
+			sl->children[i]->bind_names(sl->children[i], automatic_var_offset*OFFSET_SIZE);
+			--automatic_var_offset;
 		} else {
 			sl->children[i]->bind_names(sl->children[i], 0);
 		}
@@ -75,11 +87,14 @@ bind_declaration_list ( node_t *root, int stackOffset)
 {
 	if(outputStage == 6)
 		fprintf ( stderr, "DECLARATION_LIST: Start\n");
+	
+	/* stack offset will contain the last stack offset in this list */
 
-
-	for(int i = 0; i < root->n_children; ++i)
-		if(root->children[i] != NULL)
-			root->children[i]->bind_names(root->children[i], stackOffset + 1);
+	stackOffset += root->n_children;
+	for(int i = 0; i < root->n_children; ++i) {
+		root->children[i]->bind_names(root->children[i], stackOffset * OFFSET_SIZE);
+		--stackOffset;
+	}
 
 	if(outputStage == 6)
 		fprintf ( stderr, "DECLARATION_LIST: End\n");
@@ -91,11 +106,16 @@ bind_class(node_t *root, int stackOffset)
 {
 	if(outputStage == 6)
 		fprintf(stderr, "CLASS: Start: %s\n", root->children[0]->label);
-
-	for(int i = 0; i < root->n_children; ++i)
-		if(root->children[i] != NULL)
-			root->children[i]->bind_names(root->children[i], stackOffset + 1);
 	
+	/* declaration list, skipped... */
+	node_t *dl = root->children[0];
+
+	/* function list */
+	node_t *fl = root->children[1];
+
+	/* iterate through function list */
+	for(int i = 0; i < fl->n_children; ++i)
+		fl->children[i]->bind_names(fl->children[i], 3);
 
 	if(outputStage == 6)
 			fprintf(stderr, "CLASS: End\n");
@@ -111,10 +131,9 @@ int bind_function_list ( node_t *root, int stackOffset)
 {
 	if(outputStage == 6)
 		fprintf ( stderr, "FUNCTION_LIST: Start\n");
-
+	/* these are all functions*/
 	for(int i = 0; i < root->n_children; ++i)
-		if(root->children[i] != NULL)
-			root->children[i]->bind_names(root->children[i], stackOffset + 1);
+		root->children[i]->bind_names(root->children[i], 2);
 	
 
 	if(outputStage == 6)
@@ -156,11 +175,31 @@ int bind_expression( node_t* root, int stackOffset)
 {
 	if(outputStage == 6)
 		fprintf( stderr, "EXPRESSION: Start: %s\n", root->expression_type.text);
+	switch(root->expression_type.index) {
+	case CLASS_FIELD_E:
+		root->children[0]->bind_names(root->children[0], 0);
+		break;
+	case FUNC_CALL_E:
+		/*
+		 * Its a function call. Skip the variable access and bind
+		 * the expression list.
+		 */
+		if(root->children[1] != NULL)
+			root->children[1]->bind_names(root->children[1], 0);
+			
+		break;		
+	case METH_CALL_E:
+		root->children[0]->bind_names(root->children[0], 0);
+		
+		if(root->children[2] != NULL)
+			root->children[2]->bind_names(root->children[2], 0);
 
-	for(int i = 0; i < root->n_children; ++i)
-		if(root->children[i] != NULL)
-			root->children[i]->bind_names(root->children[i], stackOffset + 1);
-	
+		break;
+	default:
+		for(int i = 0; i < root->n_children; ++i)
+			if(root->children[i] != NULL)
+				root->children[i]->bind_names(root->children[i], stackOffset + 1);
+	}
 
 	if(outputStage == 6)
 		fprintf( stderr, "EXPRESSION: End\n");
