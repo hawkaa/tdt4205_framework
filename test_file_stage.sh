4fb60ae2975f79188b2fe87a7617dede5c6fd9fd
Håkon Åmdal <hakon@aamdal.com>
2014-02-25 11:19:20 +0100

    Nytt testscript med diff

--- test_file_stage.sh
+++ test_file_stage.sh
@@ -6,6 +6,11 @@ cp "correct_output/$1.s$2" "test_file_stage.d/$1.s$2"
 ./bin/vslc -s $2 < vsl_programs/$1.vsl 2> test_file_stage.d/$1.test.s$2
 
 echo ""
+echo "-- DIFF --"
+diff test_file_stage.d/$1.s$2 test_file_stage.d/$1.test.s$2 | cat
+
+echo ""
+echo ""
 echo "-- CORRECT OUTPUT --"
 cat test_file_stage.d/$1.s$2
 
