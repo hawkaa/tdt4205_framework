5b965816886a969e944cd5532a4499ff331c8b64
Håkon Åmdal <hakon@aamdal.com>
2014-02-26 18:25:08 +0100

    ferdig for dagen

--- src/typecheck.c
+++ src/typecheck.c
@@ -18,6 +18,10 @@ void type_error(node_t* root){
 
 data_type_t typecheck_default(node_t* root)
 {
+	for(int i = 0; i < root->n_children; ++i) {
+		if(root->children[i] != NULL)
+			root->children[i]->typecheck(root->children[i]);
+	}
 
 }
 
@@ -26,11 +30,25 @@ data_type_t typecheck_expression(node_t* root)
 
 	if(outputStage == 10)
 		fprintf( stderr, "Type checking expression %s\n", root->expression_type.text);
+	
+	data_type_t op1, op2;
+	switch(root->expression_type.text) {
+	case "+":
+		op1 = root->children[0]->typecheck(root->children[0]);
+		op2 = root->children[1]->typecheck(root->children[1]);
+		if(op1.base_type == INT_TYPE && op2.base_type == INT_TYPE) {
+			
+		} else if (op1.base_type == FLOAT_TYPE && op2.base_type == FLOAT TYPE) {
 
+		} else {
+			type_error(root);
+		}
+			
+	}
 	
 }
 
 data_type_t typecheck_variable(node_t* root)
 {
-
+	return root->entry->type;
 }
